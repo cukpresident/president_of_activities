b2e475d1d3ae33903717750259754b2d5504ab4a
Kim Ah Hyoung <llmooon@naver.com>
2019-05-21 00:40:14 +0900

    Revert "Feature/frontend/dashboard"

--- frontEnd/coreui-free-react-admin-template-master/coreui-free-react-admin-template-master/src/views/Pages/Mypage/Mypagedetail.js
+++ frontEnd/coreui-free-react-admin-template-master/coreui-free-react-admin-template-master/src/views/Pages/Mypage/Mypagedetail.js
@@ -16,9 +16,6 @@ import {
 } from '@coreui/react';
 import { inject, observer } from 'mobx-react';
 
-import './Mypage.css';
-
-const MyinfoDetail = React.lazy(()=>import('./MyInfoDetail'));
 const DefaultHeader = React.lazy(() => import('./HomeHeader'));
 
 
@@ -91,7 +88,7 @@ class Mypagedetail extends Component {
 
 
   render() {
-    const { user } = this.props;
+    const {user} = this.props.user;
     return (    
         <div className="animated fadeIn">
             <Row>
@@ -105,8 +102,8 @@ class Mypagedetail extends Component {
 
             <Col xs="12" lg="12">
                   <Card>
-                  <CardHeader style={{backgroundColor: "#e6ee9c"}}>
-                    <h5>about {user.username}</h5>
+                  <CardHeader>
+                    <h5>about {this.props.user.username}</h5>
                   </CardHeader>
                   <CardBody>
                     <Row>
@@ -116,8 +113,58 @@ class Mypagedetail extends Component {
                       </Col>
                       <Col xs="12" lg = "10">
                         
-                        <MyinfoDetail/>
-
+                      <Table responsive>
+                        <thead>
+                        <tr>
+                          <th>Username</th>
+                          <th>Date registered</th>
+                          <th>Role</th>
+                          <th>Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                          <td>Samppa Nori</td>
+                          <td>2012/01/01</td>
+                          <td>Member</td>
+                          <td>
+                            <Badge color="success">Active</Badge>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Estavan Lykos</td>
+                          <td>2012/02/01</td>
+                          <td>Staff</td>
+                          <td>
+                            <Badge color="danger">Banned</Badge>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Chetan Mohamed</td>
+                          <td>2012/02/01</td>
+                          <td>Admin</td>
+                          <td>
+                            <Badge color="secondary">Inactive</Badge>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Derick Maximinus</td>
+                          <td>2012/03/01</td>
+                          <td>Member</td>
+                          <td>
+                            <Badge color="warning">Pending</Badge>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Friderik Dávid</td>
+                          <td>2012/01/21</td>
+                          <td>Staff</td>
+                          <td>
+                            <Badge color="success">Active</Badge>
+                          </td>
+                        </tr>
+                        </tbody>
+                      </Table>
                       </Col>
                     </Row> 
                   </CardBody>
